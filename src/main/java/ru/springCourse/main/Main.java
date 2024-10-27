0007d04701de73513c398795234307038c0543ad
Ilshat Ablyuzin <revan931@yandex.ru>
2024-10-27 21:24:01 +0500

    inject anotation @value

--- src/main/java/ru/springCourse/main/Main.java
+++ src/main/java/ru/springCourse/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-app-config.xml");
         MusicPlayer firstPlayer = context.getBean(MusicPlayer.class);
+        System.out.println(firstPlayer.getName() + " : " + firstPlayer.getVolume());
         firstPlayer.play();
         context.close();
     }
--- src/main/java/ru/springCourse/main/MusicPlayer.java
+++ src/main/java/ru/springCourse/main/MusicPlayer.java
@@ -2,14 +2,17 @@ package ru.springCourse.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     private Music music;
 
+    @Value("${musicPlayer.name}")
     private String name;
 
+    @Value("${musicPlayer.volume}")
     private int volume;
 
     public MusicPlayer() {
